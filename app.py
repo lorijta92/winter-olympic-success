8b9cf5c9a1d666612ca981f1dd8eec696d8eed26
dpkeyes <dpkeyes@gmail.com>
2019-09-19 21:05:02 -0700

    getting ready for sqllite

--- app.py
+++ app.py
@@ -1,26 +1,45 @@
-# Import dependencies
+#################################################
+# Dependencies and Setup
+#################################################
+
 from flask import Flask, render_template, jsonify, request # Lori/Sergio: not sure if we need jsonify.
 from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
 
 # Create an instance of Flask
 app = Flask(__name__)
 
-# Configure our Flask instance to postgresql database (note that the server is not the default 5432. It is 5433.)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/gdpolympics'
+#################################################
+# Database Setup
+#################################################
+
+# Configure our Flask instance to sqllite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/bellybutton.sqlite'
 
 # Create database object using SQLAlchemy
 db = SQLAlchemy(app)
 
-# ----------------------------------------------------------------------
-# Create our db model
+# reflect an existing database into a new model
+Base = automap_base()
 
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
 
+# Save references to each table
+Winter = Base.classes.winter
+Summer = Base.classes.summer
+Athlete = Base.classes.athlete
+Regions = Base.classes.regions
+Country = Base.classes.country
+Soccer = Base.classes.soccer
 
-
-
-
-# ----------------------------------------------------------------------
-
+#################################################
+# Route Setup
+#################################################
 
 # Route to render index.html template
 @app.route("/")
