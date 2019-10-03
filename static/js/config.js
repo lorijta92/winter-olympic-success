15bd2869e69c2056b28487c03c1924398e9db714
Lori Ta <lorijta@gmail.com>
2019-10-03 11:11:32 -0700

    add comment to specify public token

--- static/js/config.js
+++ static/js/config.js
@@ -1,1 +1,2 @@
-API_KEY = "pk.eyJ1IjoiZHBrZXllcyIsImEiOiJjazBlbGx6Mngwajg5M2huNXJ5a3Z4Zmg0In0.7Sn3qTU7rMk_pDjpemUneQ"
+// Default public token
+API_KEY = "pk.eyJ1IjoibG9yaWp0YSIsImEiOiJjazBla3d2d2cwMGtkM29vMW92N3h6cHZ0In0.oYyCzEIU0mcd8ws1z2UZrA"
